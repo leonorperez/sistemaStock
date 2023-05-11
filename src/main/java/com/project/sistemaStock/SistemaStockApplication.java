41417758eb85a3ed908dc209eac60e3ccac3759f
Leonor Perez <leonzia_8@hotmail.com>
2023-05-10 21:01:31 -0300

    feat: getById ok and UserDTO creation.

--- src/main/java/com/project/sistemaStock/SistemaStockApplication.java
+++ src/main/java/com/project/sistemaStock/SistemaStockApplication.java
@@ -1,4 +1,4 @@
-package com.project.sistemaStock.model;
+package com.project.sistemaStock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/project/sistemaStock/controller/UserController.java
+++ src/main/java/com/project/sistemaStock/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.project.sistemaStock.controller;
 
+import com.project.sistemaStock.dto.UserDTO;
 import com.project.sistemaStock.model.User;
 import com.project.sistemaStock.repository.IUserRepository;
 import lombok.AllArgsConstructor;
@@ -8,7 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static com.project.sistemaStock.security.WebSecurityConfig.passwordEncoder;
 
@@ -19,17 +22,54 @@ public class UserController {
     @Autowired
     private final IUserRepository iUserRepository;
 
-    @GetMapping(value="/users")
-    public List<User> listUsers(){
-        System.out.println("´paso x aca");
-        return iUserRepository.findAll();
+    @GetMapping(value = "/users")
+    public List<UserDTO> listUsers() {
+        List<User> users = iUserRepository.findAll();
+        List<UserDTO> userDTOS = new ArrayList<>();
 
+        for (User user : users) {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setName(user.getName());
+            userDTO.setSurname(user.getSurname());
+            userDTO.setDni(user.getDni());
+            userDTO.setEmail(user.getEmail());
+            userDTO.setPhone(user.getPhone());
+            userDTOS.add(userDTO);
+        }
+
+        return userDTOS;
     }
 
     @PostMapping(value = "/user/new")
-    public ResponseEntity<?> save(@RequestBody User user){
+    public ResponseEntity<?> save(@RequestBody User user) {
         User newUser = new User(user.getName(), user.getSurname(), user.getDni(), user.getEmail(), user.getPhone(), passwordEncoder().encode(user.getPassword()));
-        return new ResponseEntity<>(iUserRepository.save(newUser), HttpStatus.OK);
+        newUser = iUserRepository.save(newUser);
+        UserDTO userDTO = new UserDTO();
+        userDTO.setName(newUser.getName());
+        userDTO.setSurname(newUser.getSurname());
+        userDTO.setEmail(newUser.getEmail());
+        userDTO.setDni(newUser.getDni());
+        userDTO.setPhone(newUser.getPhone());
+
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable int id) {
+        Optional<User> optionalUser = iUserRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            UserDTO userDTO = new UserDTO();
+            userDTO.setName(user.getName());
+            userDTO.setSurname(user.getSurname());
+            userDTO.setEmail(user.getEmail());
+            userDTO.setDni(user.getDni());
+            userDTO.setPhone(user.getPhone());
+
+            return new ResponseEntity<>(userDTO, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+        }
     }
 
 
